53b3da1b18fa293ce6deba153453b6926c04ceb2
unknown <pakphil7@gmail.com>
2020-07-20 15:00:18 -0700

    changed routes undid api/routes

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public/index.html"));
+  app.use(express.static(path.join(__dirname, "../build")));
+
+  app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
 }
 
 app.use(routes);
