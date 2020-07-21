1b6e08a639ca02ab9f60b09fed454173d323fb3f
unknown <pakphil7@gmail.com>
2020-07-21 13:18:48 -0700

    re re re re re re re deploying

--- server.js
+++ server.js
@@ -11,10 +11,12 @@ app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "./build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "./build", "index.html"));
 });
 
+//
+//
 //
 //
 //
