cb46be3511a906c0fb3c1c4f2b66b97b5984762a
unknown <pakphil7@gmail.com>
2020-07-20 18:50:26 -0700

    changed settings for prod

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../build", "index.html"));
-  });
-}
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+});
 
 app.use(routes);
 
